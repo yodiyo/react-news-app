6e65d14465660b675ff2d894cafae776255d015b
Yorick Brown <yorick.brown@angrycreative.uk>
2024-01-27 19:20:33 +0000

    Build out api and fetch data and add to component. Fix dependency issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SelectComponent from './components/SelectComponent';
+import { fetchNewsData } from './components/api';
 
-const NewsComponent = () => {
-	const [ selectedOption, setSelectedOption ] = useState( '' );
+const App = () => {
+	const [ selectedCountry, setSelectedCountry ] = useState( '' );
+	const [ newsData, setNewsData ] = useState( null );
 
 	const options = [
 		{ value: 'gb', label: 'United Kingdom' },
@@ -12,18 +14,34 @@ const NewsComponent = () => {
 		{ value: 'in', label: 'India' }
 	];
 
+	useEffect( () => {
+		if ( selectedCountry ) {
+			fetchNewsData( selectedCountry )
+				.then( ( data ) => setNewsData( data ) )
+				.catch( ( error ) => console.error( 'Error setting news data: ', error ) );
+		}
+	})
+
 	const handleSelectChange = e => {
-		setSelectedOption( e.target.value );
+		setSelectedCountry( e.target.value );
 	}
 
 	return (
 		<div>
 			<h2>News Headlines from Around the World</h2>
 			<SelectComponent options={ options } onChange={ handleSelectChange } />
-			<p>selected option: { selectedOption }</p>
+			{ newsData && (
+				<div>
+					<h3>Top headlines in [COUNTRY]</h3>
+					<ul>
+						{ newsData.articles.map( ( article, index ) => (
+						<li key={ index }>{ article.title }</li>
+						) ) }
+					</ul>
+				</div>
+			) }
 		</div>
-	)
-}
+	);
+};
 
-
-export default NewsComponent;
+export default App;
