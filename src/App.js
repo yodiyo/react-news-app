f2f0a965c1f6e7c4f21d50a854bfd2eccee51830
Yorick Brown <yorick.brown@angrycreative.uk>
2024-04-23 00:47:38 +0100

    add date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import SelectComponent from './components/SelectComponent';
 import { fetchNewsData } from './components/api';
-
+import { FormattedDate } from './components/formattedDate';
 
 /**
  * React component displaying the top headlines based on the selected country.
@@ -29,7 +29,24 @@ const App = () => {
 	 *
 	 * @type { Object }
  	 */
-	const [ newsData, setNewsData ] = useState( null );
+	// const [ newsData, setNewsData ] = useState( null );
+	const newsData = {
+		"totalArticles": 54904,
+		"articles": [
+		  {
+			"title": "Google's Pixel 7 and 7 Pro’s design gets revealed even more with fresh crisp renders",
+			"description": "Now we have a complete image of what the next Google flagship phones will look like. All that's left now is to welcome them during their October announcement!",
+			"content": "Google’s highly anticipated upcoming Pixel 7 series is just around the corner, scheduled to be announced on October 6, 2022, at 10 am EDT during the Made by Google event. Well, not that there is any lack of images showing the two new Google phones, b... [1419 chars]",
+			"url": "https://www.phonearena.com/news/google-pixel-7-and-pro-design-revealed-even-more-fresh-renders_id142800",
+			"image": "https://m-cdn.phonearena.com/images/article/142800-wide-two_1200/Googles-Pixel-7-and-7-Pros-design-gets-revealed-even-more-with-fresh-crisp-renders.jpg",
+			"publishedAt": "2022-09-28T08:14:24Z",
+			"source": {
+			  "name": "PhoneArena",
+			  "url": "https://www.phonearena.com"
+			}
+		  }
+		]
+	  };
 
 	/**
  	 * Countries for the select box.
@@ -44,6 +61,11 @@ const App = () => {
 		{ value: 'in', label: 'India' }
 	];
 
+	/**
+ 	 * Topics for the select box.
+	 *
+	 * @type { Array }
+ 	 */
 	const topicOptions = [
 		{ value: 'general', label: 'General' },
 		{ value: 'world', label: 'World' },
@@ -63,7 +85,7 @@ const App = () => {
 		if ( selectedCountry && selectedTopic ) {
 			fetchNewsData( selectedCountry, selectedTopic )
 				.then( ( data ) => {
-          setNewsData( data );
+          //  setNewsData( data );
           console.log(data);
         })
 			.catch( ( error ) => console.error( 'Error setting news data: ', error ) );
@@ -85,7 +107,7 @@ const App = () => {
 	}
 
 	return (
-		<section>
+		<>
 			<header>
 				<h1>News Headlines from Around the World</h1>
 			</header>
@@ -116,13 +138,14 @@ const App = () => {
 							{/* No alt tag available, so don't send image to a11y api.*/}
 							<img src={ article.image } alt="" aria-hidden="true" />
 							<a href={ article.url }>{ article.title }</a>
+							<p>{ FormattedDate( article.publishedAt ) }</p>
 							<p>{ article.description }</p>
 						</li>
 						) }
 					</ul>
 				</section>
 			}
-		</section>
+		</>
 	);
 };
 
---
+++ src/components/formattedDate.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param { string } date
+ * @return string
+ */
+const FormattedDate = ( date ) => {
+	const dateObj = new Date(date);
+	const lang = navigator.language;
+	console.log(dateObj, lang);
+
+	/** Using Intl.DateTimeFormat to output friendly date and time.
+	 * More info https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat.
+	*/
+	const formattedDate = new Intl.DateTimeFormat('navigator.language', {
+		hour: 'numeric',
+		minute: 'numeric',
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	}).format(dateObj);
+
+	return formattedDate;
+};
+
+// export default FormattedDate;
+export { FormattedDate };
