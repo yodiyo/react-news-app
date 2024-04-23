2245c13efe1d75853945f9c5e4b039c2a51f50e5
Yorick Brown <yorick.brown@angrycreative.uk>
2024-04-23 19:27:35 +0100

    add limited cached version

--- src/js/components/formattedDate.js
+++ src/js/components/formattedDate.js
@@ -3,10 +3,9 @@
  * @param { string } date
  * @returns string
  */
-const FormattedDate = ( date ) => {
+const formattedDate = ( date ) => {
 	const dateObj = new Date(date);
 	const lang = navigator.language;
-	console.log(dateObj, lang);
 
 	/** Using Intl.DateTimeFormat to output friendly date and time.
 	 * More info https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat.
@@ -26,4 +25,4 @@ const FormattedDate = ( date ) => {
 };
 
 // export default FormattedDate;
-export { FormattedDate };
+export { formattedDate };
--- src/js/news-app.js
+++ src/js/news-app.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import SelectComponent from './components/SelectComponent';
 import { fetchNewsData } from './components/api';
-import { FormattedDate } from './components/formattedDate';
+import { formattedDate } from './components/formattedDate';
 
 /**
  * React component displaying the top headlines based on the selected country.
@@ -30,23 +30,10 @@ const NewsApp = () => {
 	 * @type { Object }
  	 */
 	const [ newsData, setNewsData ] = useState( null );
-	/* const newsData = {
-		"totalArticles": 54904,
-		"articles": [
-		  {
-			"title": "Google's Pixel 7 and 7 Pro’s design gets revealed even more with fresh crisp renders",
-			"description": "Now we have a complete image of what the next Google flagship phones will look like. All that's left now is to welcome them during their October announcement!",
-			"content": "Google’s highly anticipated upcoming Pixel 7 series is just around the corner, scheduled to be announced on October 6, 2022, at 10 am EDT during the Made by Google event. Well, not that there is any lack of images showing the two new Google phones, b... [1419 chars]",
-			"url": "https://www.phonearena.com/news/google-pixel-7-and-pro-design-revealed-even-more-fresh-renders_id142800",
-			"image": "https://m-cdn.phonearena.com/images/article/142800-wide-two_1200/Googles-Pixel-7-and-7-Pros-design-gets-revealed-even-more-with-fresh-crisp-renders.jpg",
-			"publishedAt": "2022-09-28T08:14:24Z",
-			"source": {
-			  "name": "PhoneArena",
-			  "url": "https://www.phonearena.com"
-			}
-		  }
-		]
-	  };*/
+
+	// Declare a ref object
+	const selectedCountryRef = useRef({});
+	const selectedTopicRef = useRef({});
 
 	/**
  	 * Countries for the select box.
@@ -83,13 +70,19 @@ const NewsApp = () => {
      */
 	useEffect( () => {
 		if ( selectedCountry && selectedTopic ) {
-			fetchNewsData( selectedCountry, selectedTopic )
-				.then( ( data ) => {
-          setNewsData( data );
-          console.log(data);
-        })
-			.catch( ( error ) => console.error( 'Error setting news data: ', error ) );
+			console.log(selectedCountryRef.current);
+			console.log(selectedTopicRef.current);
 
+			if ( ! selectedCountryRef.current[ selectedCountry ] && ! selectedTopicRef.current[ selectedTopic ] ){
+				fetchNewsData( selectedCountry, selectedTopic )
+				.then( ( data ) => {
+					selectedCountryRef.current = selectedCountry;
+					selectedTopicRef.current = selectedTopic;
+					setNewsData( data );
+					console.log(data);
+				})
+				.catch( ( error ) => console.error( 'Error setting news data: ', error ) );
+			}
 		}
 	}, [ selectedCountry, selectedTopic ] );
 
@@ -141,9 +134,11 @@ const NewsApp = () => {
 						<li key={ index }>
 							{/* No alt tag available, so don't send image to a11y api.*/}
 							<img src={ article.image } alt="" aria-hidden="true" />
+							<p>{ article.source.name }</p>
 							<a href={ article.url }>{ article.title }</a>
-							<p>{ FormattedDate( article.publishedAt ) }</p>
+							<p>{ formattedDate( article.publishedAt ) }</p>
 							<p>{ article.description }</p>
+
 						</li>
 						) }
 					</ul>
