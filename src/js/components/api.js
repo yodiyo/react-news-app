94905ca651da9a2a4daf6ed8b01df92162a14497
Yorick Brown <yorick.brown@angrycreative.uk>
2024-04-24 21:30:42 +0100

    change apikey

--- src/js/components/api.js
+++ src/js/components/api.js
@@ -8,8 +8,8 @@
  */
 const fetchNewsData = ( country, topic ) => {
 	// const apiKey = '73b11fd29d8f3c08c1e155cd289c498d';
-	const apiKey = 'e4b936ab244da3ae1b97fec4f8263641';
-	// const apiKey = '595f0c3a5404839eb74c6c0cd118bd85';
+	// const apiKey = 'e4b936ab244da3ae1b97fec4f8263641';
+	const apiKey = '595f0c3a5404839eb74c6c0cd118bd85';
 	const apiUrl = `https://gnews.io/api/v4/top-headlines?country=${ country }&category=${topic}&apikey=${ apiKey }&max=10`;
 
 	return fetch(
